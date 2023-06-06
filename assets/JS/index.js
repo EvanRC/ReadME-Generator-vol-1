51f9bb8e66884db227ea839629c4501dcf83a00f
Evan Carlson <evanrc1@gmail.com>
2023-06-05 18:23:27 -0700

    added requirements and some array questions to help generate the readme

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,7 +1,32 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs =  require('fs');
+const GenND = require('./utiles/generatemarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Do you want a title for your project?',
+    },
+    {
+        type: 'input',
+        Name: 'Installation',
+        message: 'Do you want an installation guide section?',
+
+    },
+    {
+        type: 'input',
+        name: 'Credits',
+        message: 'Do you want a Project credits section?',
+
+    },
+    {
+        type: 'input',
+        name: 'Features',
+        message: 'Do you want a Features section?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
