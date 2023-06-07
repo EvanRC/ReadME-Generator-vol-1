88d5480bfdec7b1a2e35c65c3c38e8074d61efff
Evan Carlson <evanrc1@gmail.com>
2023-06-06 17:15:04 -0700

    added functions for the license badge and links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    switch (license) {
+      case 'MIT':
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+
+        case 'Apache 2.0':
+          return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+
+          case 'GPL-2.0':
+          return '[![License: GPL-2.0](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+
+          default:
+            return'';
+    }
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    switch (license) {
+      case 'MIT': 
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+
+      case 'Apache 2.0':
+        return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+
+        case 'GPL-2.0':
+          return '[![License: GPL-2.0](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+
+          default:
+            return'';
+    }
+  
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenseBadge = renderLicenseBadge(license);
+  const licenseLink = renderLicenseLink(license);
+
+  if (licenseBadge && licenseLink) {
+    return `
+    ## License
+    ${licenseBadge}
+    
+    ${licenseLink}
+    `;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenseSection = renderLicenseSection(data.license);
+  
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Features
+  ${data.features}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Credits
+  ${data.usage}
 
 `;
 }
